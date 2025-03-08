8de54dea1d277be5c0ea8c115c7fdbfe81715017
silvjoao <dujoao14@gmail.com>
2025-03-08 20:30:24 -0300

    ajustando

--- calculatefinancial/calculatefinancial/settings.py
+++ calculatefinancial/calculatefinancial/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config
 import environ
 import dj_database_url
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
                            
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
